9f74ad3d4c7cb32e80ba3af0b12eae6458e06c6e
Gina Sprint <sprint@gonzaga.edu>
2026-02-05 15:02:40 -0800

    inheritance lab

---
+++ animal_tester.py
@@ -0,0 +1,54 @@
+class Animal:
+    """Docstring for Animal
+    """
+    def __init__(self, species: str = "default species", age: int = 0) -> None:
+        self._species = species
+        self._age = age
+
+    def __str__(self) -> str:
+        return f"a {self._age}-year old {self._species}"
+    
+class Pet(Animal): # Pet is-a Animal (it inherits from Animal)
+    """Docstring for Pet
+    """
+    def __init__(self, species: str, age: int, name: str = "default name") -> None:
+        super().__init__(species, age) # invoke Animal's initiazer
+        # (let Animal initialize Animal-specific state)
+        self._name = name # Pet initializes Pet-specific state
+
+    def __str__(self) -> str:
+        str_rep = super().__str__()
+        str_rep += " named " + self._name
+        return str_rep
+    
+class WildAnimal(Animal): # WildAnimal is-a Animal (it inherits from Animal)
+    """Docstring for WildAnimal
+    """
+    def __init__(self, species: str, age: int, noise: str = "default noise") -> None:
+        super().__init__(species, age) # invoke Animal's initiazer
+        # (let Animal initialize Animal-specific state)
+        self._ferocious_noise = noise # WildAnimal initializes WildAnimal-specific state
+
+    def __str__(self) -> str:
+        str_rep = super().__str__()
+        str_rep += " that goes " + self._ferocious_noise
+        return str_rep
+    
+def main():
+    a1 = Animal()
+    print(a1)
+    a2 = Animal("cat", 2)
+    print(a2)
+
+    p1 = Pet("dog", 5, "spike")
+    print(p1)
+    w1 = WildAnimal("lion", 10, "roooooar!!!")
+    print(w1)
+    print()
+
+    animals = [a1, a2, p1, w1] # list of Animal references
+    for animal in animals:
+        print(animal) # same code, different behavior!!!!!
+
+
+main()
